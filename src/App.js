6b339e663c25975cae4984792f8e03f7b31a4cff
Oluwanifemi Olotu <olotuolu@msu.edu>
2023-04-30 12:18:24 -0400

    bug fixes for netlify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { supabase } from './supabaseClient';
 import 'tufte-css/tufte.css';
-import logo from './logo.svg';
 import './App.css';
 
 function Sedanlibrary() {
